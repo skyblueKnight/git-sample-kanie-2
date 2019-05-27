b92b83eb492af739309d4284f27f7130d100f9ac
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-05-27 13:56:19 +0900

    Carクラスを作成しました。

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Car {
+	
+	private String name;
+	private int speed;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
